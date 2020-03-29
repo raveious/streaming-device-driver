5d971c9398b72681c00ee732d96e86646874096c
Ian Wakely <raveious.irw@gmail.com>
2020-03-29 17:49:22 -0400

    Making a thread for device simulation, and reading

--- driver/streamer.c
+++ driver/streamer.c
@@ -4,6 +4,9 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
+#include <linux/kthread.h>
+#include <linux/random.h>
+#include <linux/delay.h>
 
 #define DEVICE_NAME "myStreamer"
 #define CLASS_NAME  "myStreamer"
@@ -25,14 +28,38 @@ static int dev_major_number;
 static struct class* dev_class = NULL;
 static struct device* dev_device = NULL;
 
-static int open_instances = 0;
-
 static struct file_operations fops = {
 	.read = device_read,
 	.write = device_write,
 	.open = device_open,
 	.release = device_release
 };
+
+static struct task_struct* device_sim_struct;
+static struct completion read_buffered_data;
+
+static int device_sim_thread(void* data) {
+    unsigned long timeout;
+    (void) data; // Unused.
+
+    printk(KERN_INFO "%s: Device simulation thread has been started.\n", DEVICE_NAME);
+
+    while (!kthread_should_stop()) {
+        // wait for the buffer to be emptied
+        timeout = wait_for_completion_timeout(&read_buffered_data, msecs_to_jiffies(500));
+
+        // If there was a timeout on the wait, just go back to waiting.
+        if (timeout == 0) {
+            continue;
+        }
+
+        printk(KERN_INFO "%s: Generated random data for simulate data read in from a device.\n", DEVICE_NAME);
+    }
+
+    printk(KERN_INFO "%s: Device simulation thread has been stopped.\n", DEVICE_NAME);
+
+    return SUCCESS;
+}
 
 int init_module(void) {
     printk(KERN_INFO "%s: Module init start.\n", DEVICE_NAME);
@@ -44,6 +71,7 @@ int init_module(void) {
         return dev_major_number;
     }
 
+    // Create the device class
     dev_class = class_create(THIS_MODULE, CLASS_NAME);
     if (IS_ERR(dev_class)) {
         unregister_chrdev(dev_major_number, DEVICE_NAME);
@@ -51,12 +79,25 @@ int init_module(void) {
         return PTR_ERR(dev_class);
     }
 
+    // Create the device itself
     dev_device = device_create(dev_class, NULL, MKDEV(dev_major_number, 0), NULL, DEVICE_NAME);
     if (IS_ERR(dev_device)) {
         class_destroy(dev_class);
         unregister_chrdev(dev_major_number, DEVICE_NAME);
         printk(KERN_ALERT "%s: Failed to create device.\n", DEVICE_NAME);
         return PTR_ERR(dev_device);
+    }
+
+    init_completion(&read_buffered_data);
+
+    device_sim_struct = kthread_run(device_sim_thread, NULL, "dev_sim_thread");
+
+    // Something bad happened when making the thread...
+    if (IS_ERR(device_sim_struct)) {
+        printk(KERN_ALERT "%s: Failed to create device simulation thread from open call.\n", DEVICE_NAME);
+
+        // Capture the error.
+        return PTR_ERR(device_sim_struct);
     }
 
     printk(KERN_INFO "%s: Module init complete.\n", DEVICE_NAME);
@@ -67,6 +108,10 @@ int init_module(void) {
 void cleanup_module(void) {
     printk(KERN_INFO "%s: Module exit.\n", DEVICE_NAME);
 
+    // Stop the thread
+    kthread_stop(device_sim_struct);
+
+    // Break everything down on exit
     device_destroy(dev_class, MKDEV(dev_major_number, 0));
     class_unregister(dev_class);
     class_destroy(dev_class);
@@ -77,23 +122,24 @@ void cleanup_module(void) {
 static int device_open(struct inode* inode, struct file* fd) {
     try_module_get(THIS_MODULE);
 
-    open_instances++;
-
     return SUCCESS;
 }
 
 static int device_release(struct inode* inode, struct file* fd) {
     module_put(THIS_MODULE);
 
-    open_instances--;
-
     return SUCCESS;
 }
 
 static ssize_t device_read(struct file* fd, char* buffer, size_t buff_size, loff_t* offset) {
+    printk(KERN_INFO "%s: App is reading data.\n", DEVICE_NAME);
+
+    // Report back saying that the buffered data has been read by someone.
+    complete(&read_buffered_data);
+
     return 0;
 }
 
 static ssize_t device_write(struct file* fd, const char* buffer, size_t buff_size, loff_t* offset) {
-    return -EINVAL;
+    return -EINVAL; // no writing...
 }
